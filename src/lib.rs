691efc9a9d4b098c160dcd234ffb018795578681
Markus Laire <malaire@gmail.com>
2026-05-15 16:09:45 +0300

    refactor: move `Downloader` to module

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,15 @@
 #![deny(missing_docs)]
 #![forbid(unsafe_code)]
 
-use std::{
-    error::Error as StdError,
-    fmt,
-    io::Error as IoError,
-    time::{Duration, Instant, SystemTime},
+use std::{error::Error as StdError, fmt, io::Error as IoError};
+
+use reqwest::{Error as ReqwestError, StatusCode};
+
+pub use crate::{
+    downloader::Downloader, downloader_builder::DownloaderBuilder, request_builder::RequestBuilder,
 };
 
-use reqwest::{blocking::Client, header::HeaderMap, Error as ReqwestError, IntoUrl, StatusCode};
-
-pub use crate::{downloader_builder::DownloaderBuilder, request_builder::RequestBuilder};
-
+mod downloader;
 mod downloader_builder;
 mod request_builder;
 mod util;
@@ -103,131 +101,5 @@ impl From<IoError> for Error {
 impl From<ReqwestError> for Error {
     fn from(error: ReqwestError) -> Self {
         Self::Reqwest(error)
-    }
-}
-
-// ======================================================================
-// Downloader - PUBLIC
-// ======================================================================
-
-/// Simple blocking downloader.
-///
-/// See [crate index](crate#examples) for examples.
-pub struct Downloader {
-    client: Client,
-    headers: HeaderMap,
-    min_delay: Duration,
-    max_delay: Duration,
-    min_interval: Duration,
-    max_interval: Duration,
-    retry_delays: Vec<(Duration, Duration)>,
-    sleep_until: Instant,
-}
-
-impl Downloader {
-    /// Creates [`DownloaderBuilder`] to configure [`Downloader`].
-    ///
-    /// This is same as [`DownloaderBuilder::new`].
-    ///
-    /// See [custom configuration] for an example.
-    ///
-    /// [custom configuration]: crate#custom-configuration
-    pub fn builder() -> DownloaderBuilder {
-        DownloaderBuilder::new()
-    }
-
-    /// Returns response headers of the latest download.
-    ///
-    /// Returned [`HeaderMap`] is empty if the latest download failed
-    /// before getting headers or if no downloads have been done yet.
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// use ml_downloader::Downloader;
-    ///
-    /// let mut downloader = Downloader::new()?;
-    /// let bytes = downloader.url("https://example.com/").get()?;
-    /// let headers = downloader.headers();
-    /// # Ok::<(), ml_downloader::Error>(())
-    /// ```
-    ///
-    /// [`HeaderMap`]: reqwest::header::HeaderMap
-    pub fn headers(&self) -> &HeaderMap {
-        &self.headers
-    }
-
-    /// Returns parsed Last-Modified header of the latest download.
-    ///
-    /// Returns `None` if
-    /// - the latest download didn't have Last-Modified header
-    /// - the latest download failed before getting headers
-    /// - no downloads have been done yet
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// use ml_downloader::Downloader;
-    ///
-    /// let mut downloader = Downloader::new()?;
-    /// let bytes = downloader.url("https://example.com/").get()?;
-    /// let mtime = downloader.modified()?.unwrap();
-    /// # Ok::<(), ml_downloader::Error>(())
-    /// ```
-    ///
-    /// # Errors
-    ///
-    /// Fails if Last-Modified header is invalid and can't be parsed.
-    pub fn modified(&self) -> Result<Option<SystemTime>, Error> {
-        Ok(util::parse_last_modified_header(&self.headers)?)
-    }
-
-    /// Creates new [`Downloader`] with default configuration.
-    pub fn new() -> Result<Self, Error> {
-        DownloaderBuilder::new().build()
-    }
-
-    /// Sleeps until ready for next download.
-    ///
-    /// After this the next [`RequestBuilder::get`] will start
-    /// download immediately without sleep.
-    ///
-    /// See [`DownloaderBuilder::delay`] and [`DownloaderBuilder::interval`].
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// use ml_downloader::Downloader;
-    ///
-    /// let mut downloader = Downloader::builder()
-    ///     .interval(1.0, 1.0)
-    ///     .build()?;
-    ///
-    /// println!("First download");
-    /// let bytes1 = downloader.url("https://example.com/first").get()?;
-    /// downloader.sleep_until_ready();
-    /// println!("Second download");
-    /// let bytes2 = downloader.url("https://example.com/second").get()?;
-    ///
-    /// # Ok::<(), ml_downloader::Error>(())
-    /// ```
-    pub fn sleep_until_ready(&mut self) {
-        let now = Instant::now();
-        if self.sleep_until > now {
-            std::thread::sleep(self.sleep_until - now);
-        }
-    }
-
-    /// Begins building a request to download file from given `url`.
-    ///
-    /// See [simple usage] and [`RequestBuilder::hash`] for examples.
-    ///
-    /// # Errors
-    ///
-    /// If given `url` is invalid then [`RequestBuilder::get`] will fail.
-    ///
-    /// [simple usage]: crate#simple-usage
-    pub fn url<U: IntoUrl>(&mut self, url: U) -> RequestBuilder<'_> {
-        RequestBuilder::new(self, self.client.get(url))
     }
 }
